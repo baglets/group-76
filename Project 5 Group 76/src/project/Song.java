f7c18b7cd1024e7823b76978ff2b12c751811da9
baglets <samuelmt@vt.edu>
2018-04-04 22:41:21 -0400

    Updated with added Song and SongTest classes

---
+++ Project 5 Group 76/src/project/Song.java
@@ -0,0 +1,103 @@
+// Virginia Tech Honor Code Pledge:
+//
+// As a Hokie, I will conduct myself with honor and integrity at all times.
+// I will not lie, cheat, or steal,
+// nor will I accept the actions of those who
+// do.
+// -- Samuel Tyson (samuelmt)
+
+package project;
+
+/**
+ * This class represents one of the songs from the file. It will have an array
+ * of responses from each student that responded to the survey.
+ *
+ * @author <Samuel Tyson> <samuelmt>
+ * @version 2018.04.04
+ */
+
+public class Song {
+
+    // Fields
+    private Response[] responses;
+    private String name;
+    private String artist;
+    private String genre;
+    private int year;
+    private int numResponses;
+    private final int MAX_RESPONSES = 100;
+
+
+    /**
+     * Default constructor.
+     * 
+     * @param n
+     *            The name of the song
+     * @param y
+     *            The year the song was written
+     * @param a
+     *            The artist of the song
+     * @param g
+     *            The genre of the song
+     */
+    public Song(String n, int y, String a, String g) {
+        responses = new Response[MAX_RESPONSES];
+        name = n;
+        year = y;
+        artist = a;
+        genre = g;
+
+        numResponses = 0;
+    }
+
+
+    /**
+     * This adds a response to the song's array of responses.
+     * 
+     * @param response
+     *            The response to add
+     */
+    public void addResponse(Response response) {
+        responses[numResponses] = response;
+        numResponses++;
+    }
+
+
+    /**
+     * This returns the number of responses, whose hobby is 'hobby', answered
+     * yes to heard.
+     * 
+     * @param hobby
+     *            The hobby to check for
+     * @return how many reponses said yes to hearing it
+     */
+    public int getHeardHobby(HobbyEnum hobby) {
+        int count = 0;
+        for (int i = 0; i < numResponses; i++) {
+            if (responses[i].getHobby().equals(hobby) && responses[i].getHeard()) {
+                count++;
+            }
+        }
+        return count;
+    }
+    
+    
+    /**
+     * This returns the number of responses, whose major is 'major', answered
+     * yes to heard.
+     * 
+     * @param major
+     *            The major to check for
+     * @return how many reponses said yes to hearing it
+     */
+    public int getHeardMajor(MajorEnum major) {
+        int count = 0;
+        for (int i = 0; i < numResponses; i++) {
+            if (responses[i].getMajor().equals(major) && responses[i].getHeard()) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
---
+++ Project 5 Group 76/src/project/SongTest.java
@@ -0,0 +1,66 @@
+// Virginia Tech Honor Code Pledge:
+//
+// As a Hokie, I will conduct myself with honor and integrity at all times.
+// I will not lie, cheat, or steal,
+// nor will I accept the actions of those who
+// do.
+// -- Samuel Tyson (samuelmt)
+
+/**
+ * This class
+ *
+ * @author <Samuel Tyson> <samuelmt>
+ * @version 2018.MM.DD
+ */
+package project;
+
+import junit.framework.TestCase;
+
+public class SongTest extends TestCase {
+
+    // Fields
+    private Song song;
+
+
+    /**
+     * Default constructor.
+     */
+    public SongTest() {
+        // Intentionally left empty
+    }
+
+
+    /**
+     * Set up.
+     */
+    public void setUp() {
+        song = new Song("1985", 2004, "Smash Mouth", "Rock");
+        song.addResponse(new Response(HobbyEnum.ART, MajorEnum.CS,
+            RegionEnum.NORTHEAST, true, true));
+        song.addResponse(new Response(HobbyEnum.ART, MajorEnum.CS,
+            RegionEnum.NORTHEAST, true, true));
+        song.addResponse(new Response(HobbyEnum.ART, MajorEnum.CS,
+            RegionEnum.NORTHEAST, true, true));
+        song.addResponse(new Response(HobbyEnum.MUSIC, MajorEnum.MATH_OR_CMDA,
+            RegionEnum.OUTSIDE, true, true));
+    }
+    
+    
+    /**
+     * This tests getting the heard for hobby
+     */
+    public void testGetHeardHobby() {
+        assertEquals(3, song.getHeardHobby(HobbyEnum.ART));
+        assertEquals(1, song.getHeardHobby(HobbyEnum.MUSIC));
+    }
+    
+    
+    /**
+     * This tests getting the heard for major
+     */
+    public void testGetHeardMajor() {
+        assertEquals(3, song.getHeardMajor(MajorEnum.CS));
+        assertEquals(1, song.getHeardMajor(MajorEnum.MATH_OR_CMDA));
+    }
+
+}
